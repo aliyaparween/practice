bf4382a16f8a915de82ca79de34b44eff35e602f
aliya-parween <aliyaparween1205@gmail.com>
2026-02-11 14:09:16 +0530

    new line added:

--- calculator/src/main/java/calculator/Addition.java
+++ calculator/src/main/java/calculator/Addition.java
@@ -5,12 +5,14 @@ public class Addition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner (System.in);
+		
 		System.out.println("Enter first number: ");
 		int a= sc.nextInt();
 		System.out.println("Enter second number: ");
 		int b=sc.nextInt();
 		int c= a+b;
 		System.out.println("Addition of "+a+" and "+b+ " is: "+c);
+		System.out.println("Calculator program ended.");
 		sc.close();
 		
 
